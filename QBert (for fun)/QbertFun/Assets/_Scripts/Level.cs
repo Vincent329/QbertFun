1aa1558d8bdb898bfad989d5e68f65658f5e3e1e
Vincent329 <vincee.ho@gmail.com>
2021-01-26 01:47:45 -0500

    general asset addition

--- QBert (for fun)/QbertFun/Assets/_Scripts/Level.cs
+++ QBert (for fun)/QbertFun/Assets/_Scripts/Level.cs
@@ -25,13 +25,22 @@ public class Level : MonoBehaviour
 
     void levelLayout()
     {
+        float tempX = prefab.position.x;
+        float tempY = prefab.position.y;
         for (int i = 0; i < row; i++)
         {
+            // column
+            //Instantiate(block, new Vector2(tempX + (-0.48f * i), tempY + (-0.721f * i)), block.transform.rotation);
+
             for (int j = 0; j < row - i; j++)
             {
                 //Instantiate();
-                Instantiate(block, new Vector2(-1.0f * j, 1.0f * i), block.transform.rotation);
+                Instantiate(block, new Vector2(tempX + (-0.48f * j), tempY - (0.721f * j)), block.transform.rotation);
+
             }
+            tempX += 0.48f;
+            tempY -= 0.721f;
+
         }
     }
 }
